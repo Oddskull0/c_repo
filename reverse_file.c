620654fd217dad5dccbe18d96edb9e34117e8402
Oddskull0 <theastronerdginger@gmail.com>
2023-11-30 18:11:37 -0700

    finished homework

--- reverse_file.c
+++ reverse_file.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
+// this fuction will swap the character of the string at postion i with character at postion j.
 void swap(char *str, int i, int j) {
     char tmp = str[i];
     str[i] = str[j];
     str[j] = tmp;
 }
-
-void reverse_stringchar(char *str, int len) {
-    int n = len;
-    int center = n / 2 ;
-    for () {
-        
+// this function will use two pointers to traverse the string, pointer (i) will increase and the pointer(j) will decrease. both pointers will stop in the middle of the string. 
+void reverse_string(char *str) {
+    int end = strlen(str);
+    int middle = end / 2 ;
+    // homw work finished
+    for (int i = 0, j = end - 1; i < middle ; i++, j--){
+        swap(str, i, j);  
     }
 }
-
+//this function will uses the reverse string function and the swap function to reverse a string. 
 int main(int argc, char** args) {
     char reverse_me[] = "dlroW olleH";
-    reverse_string(reverse_me,11);
+    reverse_string(reverse_me);
     printf("%s\n", reverse_me);
     return 0;
 }
-
-
